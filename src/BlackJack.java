9605cdb6bb3496742ba2ad92775a2cdfa1f51b4c
Andrey Kornev <andrey.kornev@dalee.ru>
2024-11-01 23:51:02 +0400

    Initial commit

---
+++ src/BlackJack.java
@@ -0,0 +1,105 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Random;
+import javax.swing.*;
+
+public class BlackJack {
+    private class Card {
+        String value;
+        String type;
+
+        Card(String value, String type) {
+            this.value = value;
+            this.type = type;
+        }
+
+        public String toString() {
+            return value + "-" + type;
+        }
+
+        public int getValue() {
+            if ("AJQK".contains(value)) {
+                if (value.equals("A")) {
+                    return 11;
+                }
+                return 10;
+            }
+            return Integer.parseInt(value);
+        }
+
+        public boolean isAce() {
+            return value.equals("A");
+        }
+    }
+
+    ArrayList<Card> deck;
+    Random random = new Random();
+
+    //dealer
+    Card hiddenCard;
+    ArrayList<Card> dealerHand;
+    int dealerSum;
+    int dealerAceCount;
+
+    BlackJack() {
+        startGame();
+    }
+
+    public void startGame() {
+        //deck
+        buildDeck();
+        shuffleDeck();
+
+        //dealer
+        dealerHand = new ArrayList<Card>();
+        dealerSum = 0;
+        dealerAceCount = 0;
+
+        hiddenCard = deck.removeLast(); // remove card at last index
+        dealerSum += hiddenCard.getValue();
+        dealerAceCount += hiddenCard.isAce() ? 1 : 0;
+
+        Card card = deck.removeLast();
+        dealerSum += card.getValue();
+        dealerAceCount += card.isAce() ? 1 : 0;
+        dealerHand.add(card);
+
+        System.out.println("DEALER");
+        System.out.println(hiddenCard);
+        System.out.println(dealerHand);
+        System.out.println(dealerSum);
+        System.out.println(dealerAceCount);
+    }
+
+    public void buildDeck() {
+        deck = new ArrayList<Card>();
+        String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        String[] types = {"C", "D", "H", "S"};
+
+        for (int i = 0; i < types.length; i++) {
+            for (int j = 0; j < values.length; j++) {
+                Card card = new Card(values[j], types[i]);
+                deck.add(card);
+            }
+        }
+
+        System.out.println("BUILD DECK:");
+        System.out.println(deck);
+
+    }
+
+    public void shuffleDeck() {
+        for (int i = 0; i< deck.size(); i++) {
+            int j = random.nextInt(deck.size());
+            Card currentCard = deck.get(i);
+            Card randomCard = deck.get(j);
+            deck.set(i, randomCard);
+            deck.set(j, currentCard);
+        }
+
+        System.out.println("AFTER SHUFFLE");
+        System.out.println(deck);
+    }
+}
